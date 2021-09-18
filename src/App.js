acd9cac56bdb087d75d79984a01838db49b5f5c1
brianpompey <brianthescholar@gmail.com>
2021-09-18 15:21:33 -0400

    Added switch for routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from './components/Header';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './default.scss';
 import Homepage from './pages/Homepage';
 import Registration from './pages/Registration';
@@ -9,8 +9,10 @@ function App() {
     <div className="App">
       <Header />
       <div className="main">
-        <Route exact path="/" component={Homepage} />
-        <Route path="/registration" component={Registration} />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/registration" component={Registration} />
+        </Switch>
       </div>
     </div>
   );
