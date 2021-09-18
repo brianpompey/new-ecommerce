96018b20b1afc42aed5f8385ee01f1a8bbedf03c
brianpompey <brianthescholar@gmail.com>
2021-09-18 14:58:53 -0400

    fixed styling on directory

--- src/components/Directory/index.js
+++ src/components/Directory/index.js
@@ -1,35 +1,35 @@
 import React from 'react';
-import Recipe from './../../assets/recipes.jpg'
-import Grill from './../../assets/grills.jpg'
+import Recipe from './../../assets/recipes.jpg';
+import Grill from './../../assets/grills.jpg';
 import "./styles.scss";
 
 const Directory = props => {
-    return(
-        <div className="Directory">
-            <div className="wrap">
-                <div
-                    className="item"
-                    style={{
-                        backgroundImage: `url(${Recipe})`
-                    }}
-                >
-                    <a>
-                        Recipes
-                    </a>
-                </div>
-                <div
-                    className="item"
-                    style={{
-                        backgroundImage: `url(${Grill})`
-                    }}
-                >
-                    <a>
-                        Grills
-                    </a>
-                </div>
-            </div>
+    return (
+      <div className="directory">
+        <div className="wrap">
+          <div
+            className="item"
+            style={{
+              backgroundImage: `url(${Grill})`
+            }}
+          >
+            <a>
+              Grills
+            </a>
+          </div>
+          <div
+            className="item"
+            style={{
+              backgroundImage: `url(${Recipe})`
+            }}
+          >
+            <a>
+              Recipes
+            </a>
+          </div>
         </div>
+      </div>
     );
-};
-
+  };
+  
 export default Directory;
