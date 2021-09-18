244d8b537527a01feed5eff34b2c82a2474afbaf
brianpompey <brianthescholar@gmail.com>
2021-09-18 15:45:53 -0400

    Adding links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles.scss';
+import { Link } from 'react-router-dom';
 import Logo from './../../assets/newbbqlogo.jpg'
 
 const Header = props => {
@@ -7,7 +8,19 @@ const Header = props => {
         <header className="header">
             <div className="wrap">
                 <div className="logo">
-                    <img src={Logo} alt="BBQ LOGO" />
+                    <Link to="/">
+                        <img src={Logo} alt="BBQ LOGO" />
+                    </Link>
+                </div>
+
+                <div className="callToActions">
+                    <ul>
+                        <li>
+                            <Link to="/registration">
+                                Register
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
 
