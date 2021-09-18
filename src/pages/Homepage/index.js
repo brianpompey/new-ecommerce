4a1fd80bb0653cc410db9a91f72830475d57400c
brianpompey <brianthescholar@gmail.com>
2021-09-18 14:26:54 -0400

    new div

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import Directory from './../../components/Directory';
 
 const Homepage = props => {
     return (
         <section>
-            <h1>
-                Homepage
-            </h1>
+            <Directory />
         </section>
     );
 };
