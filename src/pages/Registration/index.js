09c28552d7ff0dd6e0e055a58b15932ef8c5662d
brianpompey <brianthescholar@gmail.com>
2021-09-18 15:15:44 -0400

    Ading refistraitno for now

---
+++ src/pages/Registration/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.scss';
+
+class Registration extends Component {
+    render() {
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Registration;
