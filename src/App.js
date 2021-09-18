c3f93e748922ad162d259b69254904504fd172d8
brianpompey <brianthescholar@gmail.com>
2021-09-18 14:20:27 -0400

    using a new logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from './components/Header';
 import './default.scss';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h2>Hello World</h2>
+      < Homepage />
     </div>
   );
 }
