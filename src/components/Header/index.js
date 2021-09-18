a5abb3aee2ef7789611d4190ce6fdc1d2e718ac9
brianpompey <brianthescholar@gmail.com>
2021-09-18 13:36:14 -0400

    Adding styling

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './default.scss';
+
+const Header = props => {
+    return (
+        <header className="header">
+
+        </header>
+    );
+};
+
+export default Header;
