a80bcf546e598a3f93802a4642724e785c60b6e4
brianpompey <brianthescholar@gmail.com>
2021-09-18 16:51:44 -0400

    Adding redux

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './styles.scss';
 import { Link } from 'react-router-dom';
 import Logo from './../../assets/newbbqlogo.jpg'
@@ -31,4 +32,12 @@ const Header = props => {
     );
 };
 
-export default Header;
+Header.defaultProps = {
+    currentUser: null
+};
+
+const mapStateToProps = ({ user }) => ({
+    currentUser: user.currentUser
+});
+
+export default connect(mapStateToProps, null)(Header);
