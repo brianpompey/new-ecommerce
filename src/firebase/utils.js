f842d9c09f5892b81a0ddf0239ddc745c93b69a0
brianpompey <brianthescholar@gmail.com>
2021-09-18 20:52:24 -0400

    trying to figure out google auth

--- src/firebase/utils.js
+++ src/firebase/utils.js
@@ -1,4 +1,14 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-import { firebaseConfig } from "./config";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import { firebaseConfig } from './config';
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+export const GoogleProvider = new firebase.auth.GoogleAuthProvider();
+GoogleProvider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithGoogle(GoogleProvider);
