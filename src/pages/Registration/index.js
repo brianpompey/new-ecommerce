2831b4cee63c5956fd48e652b1cd52a59ab29af0
brianpompey <brianthescholar@gmail.com>
2021-09-20 12:21:12 -0400

    Added sign up to registratiion page

--- src/pages/Registration/index.js
+++ src/pages/Registration/index.js
@@ -4,11 +4,7 @@ import './styles.scss';
 
 class Registration extends Component {
     render() {
-        return(
-            <div>
-                <h1>Registration</h1>
-            </div>
-        );
+        return <SignUp />;
     }
 }
 
