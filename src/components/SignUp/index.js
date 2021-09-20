347205104ccb89cd9a46a306c9845aff2957a78f
brianpompey <brianthescholar@gmail.com>
2021-09-20 12:31:51 -0400

    Adding form

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import './styles.scss';
+import FormInput from '../forms/FormInput';
+import Button from '../forms/Button';
+
+const initialState = {
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+};
 
 class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ...initialState
+        };
+    }
 
     render() {
+        const { displayName } = this.state;
+
         return (
             <div className="signup">
                 <div className="wrap">
@@ -12,7 +29,12 @@ class SignUp extends Component {
                     </h2>
 
                     <form>
-
+                        <FormInput 
+                            type="text"
+                            name="displayName"
+                            value={displayName}
+                            placeholder="Full Name"
+                        />
                     </form>
                 </div>
             </div>
